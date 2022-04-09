4b6a15d0462e73d2b111bedd236bfc71fe36f05c
gomjellie <gomjellie@gmail.com>
2022-04-09 22:51:58 +0900

    18-easy-tuple-length

--- questions/18-easy-tuple-length/test-cases.ts
+++ questions/18-easy-tuple-length/test-cases.ts
@@ -1,7 +1,15 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
-const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+const spaceX = [
+  'FALCON 9',
+  'FALCON HEAVY',
+  'DRAGON',
+  'STARSHIP',
+  'HUMAN SPACEFLIGHT',
+] as const
+
+type Length<T> = T extends { length: infer L } ? L : never
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
@@ -9,5 +17,5 @@ type cases = [
   // @ts-expect-error
   Length<5>,
   // @ts-expect-error
-  Length<'hello world'>,
+  Length<'hello world'>
 ]
