10bcda4f6b7c31eafabc6a74d0d24c14a8e6e964
gomjellie <gomjellie@gmail.com>
2022-04-10 10:34:57 +0900

    189-easy-awaited

--- questions/189-easy-awaited/test-cases.ts
+++ questions/189-easy-awaited/test-cases.ts
@@ -4,10 +4,18 @@ type X = Promise<string>
 type Y = Promise<{ field: number }>
 type Z = Promise<Promise<string | number>>
 
+type ZV = MyAwaited<Z>
+
+type MyAwaited<T> = T extends Promise<infer Q>
+  ? Q extends Promise<unknown>
+    ? MyAwaited<Q>
+    : Q
+  : never
+
 type cases = [
   Expect<Equal<MyAwaited<X>, string>>,
   Expect<Equal<MyAwaited<Y>, { field: number }>>,
-  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>
 ]
 
 // @ts-expect-error
