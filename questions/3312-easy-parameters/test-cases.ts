c98133ad579bfac00261b0c75905a32a64864bea
gomjellie <gomjellie@gmail.com>
2022-04-10 11:43:51 +0900

    3312-easy-parameters

--- questions/3312-easy-parameters/test-cases.ts
+++ questions/3312-easy-parameters/test-cases.ts
@@ -1,11 +1,13 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type MyParameters<P> = P extends (...p: infer P) => unknown ? P : never
+
 const foo = (arg1: string, arg2: number): void => {}
-const bar = (arg1: boolean, arg2: {a: 'A'}): void => {}
+const bar = (arg1: boolean, arg2: { a: 'A' }): void => {}
 const baz = (): void => {}
 
 type cases = [
   Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
-  Expect<Equal<MyParameters<typeof bar>, [boolean, {a: 'A'}]>>,
-  Expect<Equal<MyParameters<typeof baz>, []>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>
 ]
