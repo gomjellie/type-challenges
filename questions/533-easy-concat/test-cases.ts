39fbd8273a6e1c964bc287ec845a55e70acd1d5b
gomjellie <gomjellie@gmail.com>
2022-04-10 10:54:14 +0900

    533-easy-concat

--- questions/533-easy-concat/test-cases.ts
+++ questions/533-easy-concat/test-cases.ts
@@ -1,8 +1,15 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type Concat<T extends unknown[], Q extends unknown[]> = [...T, ...Q]
+
 type cases = [
   Expect<Equal<Concat<[], []>, []>>,
   Expect<Equal<Concat<[], [1]>, [1]>>,
   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
-  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+  Expect<
+    Equal<
+      Concat<['1', 2, '3'], [false, boolean, '4']>,
+      ['1', 2, '3', false, boolean, '4']
+    >
+  >
 ]
