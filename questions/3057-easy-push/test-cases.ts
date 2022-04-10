802af6142ae51f257ddd7bba4c752cc0db77a615
gomjellie <gomjellie@gmail.com>
2022-04-10 11:33:46 +0900

    3057-easy-push

--- questions/3057-easy-push/test-cases.ts
+++ questions/3057-easy-push/test-cases.ts
@@ -1,7 +1,9 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+type Push<P extends unknown[], Q> = [...P, Q]
+
 type cases = [
   Expect<Equal<Push<[], 1>, [1]>>,
   Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
-  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>
 ]
