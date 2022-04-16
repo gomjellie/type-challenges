51c834da9fc0a54abfd5ba9a0c66fcc07500c0ba
gomjellie <gomjellie@gmail.com>
2022-04-17 03:09:17 +0900

    106-medium-trimleft

--- questions/106-medium-trimleft/test-cases.ts
+++ questions/106-medium-trimleft/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type WhiteSpace = ' ' | '\n' | '\t'
+
+type TrimLeft<S extends string> = S extends `${WhiteSpace}${infer R}`
+  ? TrimLeft<R>
+  : S
+
 type cases = [
   Expect<Equal<TrimLeft<'str'>, 'str'>>,
   Expect<Equal<TrimLeft<' str'>, 'str'>>,
@@ -7,5 +13,5 @@ type cases = [
   Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
   Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
   Expect<Equal<TrimLeft<''>, ''>>,
-  Expect<Equal<TrimLeft<' \n\t'>, ''>>,
+  Expect<Equal<TrimLeft<' \n\t'>, ''>>
 ]
