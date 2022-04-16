10309d183af0b3f89bab40f1381f067550708c9b
gomjellie <gomjellie@gmail.com>
2022-04-16 16:55:07 +0900

    16-medium-pop

--- questions/16-medium-pop/test-cases.ts
+++ questions/16-medium-pop/test-cases.ts
@@ -1,6 +1,8 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type Pop<T extends unknown[]> = T extends [...infer A, unknown] ? [...A] : never
+
 type cases = [
   Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
-  Expect<Equal<Pop<['a', 'b', 'c', 'd', ]>, ['a', 'b', 'c']>>,
+  Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>
 ]
