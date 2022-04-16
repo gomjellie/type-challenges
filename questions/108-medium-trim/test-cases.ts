cab7727091899cce8cf2c16bdd76c967524ad46b
gomjellie <gomjellie@gmail.com>
2022-04-17 03:25:47 +0900

    108-medium-trim

--- questions/108-medium-trim/test-cases.ts
+++ questions/108-medium-trim/test-cases.ts
@@ -1,5 +1,15 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type WhiteSpace = ' ' | '\n' | '\t'
+
+// prettier-ignore
+type Trim<S extends string> = 
+  S extends `${WhiteSpace}${infer M}`
+    ? Trim<M>
+    : S extends `${infer M}${WhiteSpace}`
+      ? Trim<M>
+      : S
+
 type cases = [
   Expect<Equal<Trim<'str'>, 'str'>>,
   Expect<Equal<Trim<' str'>, 'str'>>,
@@ -8,5 +18,5 @@ type cases = [
   Expect<Equal<Trim<'     str     '>, 'str'>>,
   Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
   Expect<Equal<Trim<''>, ''>>,
-  Expect<Equal<Trim<' \n\t '>, ''>>,
+  Expect<Equal<Trim<' \n\t '>, ''>>
 ]
