18153e209e19006a4090e0d3345d9464f1873beb
gomjellie <gomjellie@gmail.com>
2022-04-16 16:51:29 +0900

    15-medium-last

--- questions/15-medium-last/test-cases.ts
+++ questions/15-medium-last/test-cases.ts
@@ -1,6 +1,8 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type Last<T extends unknown[]> = T extends [...unknown[], infer B] ? B : never
+
 type cases = [
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
-  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>
 ]
