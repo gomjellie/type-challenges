928adfe4608745694b811c289629905deb1de2c7
gomjellie <gomjellie@gmail.com>
2022-04-17 19:43:55 +0900

    296-medium-permutation

--- questions/296-medium-permutation/test-cases.ts
+++ questions/296-medium-permutation/test-cases.ts
@@ -1,5 +1,15 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// prettier-ignore
+type Permutation<T, U = T> = [T] extends [never] ? [] : (
+  T extends U ? (
+    [T, ...Permutation<Exclude<U, T>>]
+  ) : []
+)
+
+type V = Permutation<'A' | 'B' | 'C'>
+
+// prettier-ignore
 type cases = [
   Expect<Equal<Permutation<'A'>, ['A']>>,
   Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
