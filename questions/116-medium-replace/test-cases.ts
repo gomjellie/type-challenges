1dcccbbb9497ebf60b33e374d4dce8d061548aea
gomjellie <gomjellie@gmail.com>
2022-04-17 16:16:58 +0900

    116-medium-replace

--- questions/116-medium-replace/test-cases.ts
+++ questions/116-medium-replace/test-cases.ts
@@ -1,9 +1,15 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// prettier-ignore
+type Replace<O extends string, S extends string, T extends string> =
+  O extends `${infer L}${S}${infer R}` ? (
+    S extends '' ? O : `${L}${T}${R}`
+  ) : O
+
 type cases = [
   Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
   Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
   Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
   Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
-  Expect<Equal<Replace<'', '', ''>, ''>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>
 ]
