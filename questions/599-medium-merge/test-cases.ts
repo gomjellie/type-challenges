21c8050a014ec4b1e3d0d0b428e238f844523d4e
gomjellie <gomjellie@gmail.com>
2022-04-30 14:22:04 +0900

    599-medium-merge

--- questions/599-medium-merge/test-cases.ts
+++ questions/599-medium-merge/test-cases.ts
@@ -1,18 +1,28 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// prettier-ignore
+type Merge<T, Q> = {
+  [k in keyof T | keyof Q]: k extends keyof Q ? Q[k] : (k extends keyof T ? T[k] : never)
+}
+
 type Foo = {
-  a: number;
-  b: string;
-};
+  a: number
+  b: string
+}
 type Bar = {
-  b: number;
-  c: boolean;
-};
+  b: number
+  c: boolean
+}
 
 type cases = [
-  Expect<Equal<Merge<Foo, Bar>, {
-	a: number;
-	b: number;
-	c: boolean;
-  }>>
+  Expect<
+    Equal<
+      Merge<Foo, Bar>,
+      {
+        a: number
+        b: number
+        c: boolean
+      }
+    >
+  >
 ]
