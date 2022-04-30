c396efbd4fe9d65c2fd7c2632e952998046920b0
gomjellie <gomjellie@gmail.com>
2022-04-30 14:12:34 +0900

    529-medium-absolute

--- questions/529-medium-absolute/test-cases.ts
+++ questions/529-medium-absolute/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// prettier-ignore
+type Absolute<T extends number | string | bigint> = 
+  `${T}` extends `${infer F}${infer Rest}` ? (
+    F extends '-' ? Rest : `${F}${Rest}`
+  ) : never
+
 type cases = [
   Expect<Equal<Absolute<0>, '0'>>,
   Expect<Equal<Absolute<-0>, '0'>>,
@@ -10,5 +16,5 @@ type cases = [
   Expect<Equal<Absolute<'10'>, '10'>>,
   Expect<Equal<Absolute<'-5'>, '5'>>,
   Expect<Equal<Absolute<-1_000_000n>, '1000000'>>,
-  Expect<Equal<Absolute<9_999n>, '9999'>>,
+  Expect<Equal<Absolute<9_999n>, '9999'>>
 ]
