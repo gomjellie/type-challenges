8073f51ff37eabd18c09d02d1e6203cc63197b24
gomjellie <gomjellie@gmail.com>
2022-04-30 14:17:22 +0900

    531-medium-string-to-union

--- questions/531-medium-string-to-union/test-cases.ts
+++ questions/531-medium-string-to-union/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// prettier-ignore
+type StringToUnion<T extends string> = 
+  T extends `${infer F}${infer Rest}` ? (
+    F | StringToUnion<Rest>
+  ) : never
+
 type cases = [
   Expect<Equal<StringToUnion<"">, never>>,
   Expect<Equal<StringToUnion<"t">, "t">>,
