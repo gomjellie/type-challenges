e46efa9a20bac631193f39bc94fdec8d203eed67
gomjellie <gomjellie@gmail.com>
2022-05-05 15:09:14 +0900

    645-medium-diff

--- questions/645-medium-diff/test-cases.ts
+++ questions/645-medium-diff/test-cases.ts
@@ -1,5 +1,27 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// prettier-ignore
+type Common<T extends Record<string, unknown>, U extends Record<string, unknown>> = 
+  T extends unknown ? {
+    [k in keyof T & keyof U]: (
+      k extends keyof T ? (
+        T[k]
+      ) : k extends keyof U ? (
+        U[k]
+      ) : never
+    )
+  } : never
+
+// prettier-ignore
+type Diff<T extends Record<string, unknown>, U extends Record<string, unknown>> =
+  (Common<T, U>) extends infer R ? (
+    Omit<T & U, keyof R> extends infer S ? {
+      [k in keyof S]: (k extends keyof T ? T[k] : (
+        k extends keyof U ? U[k]: never
+      ))
+    } : never
+  ) : never
+
 type Foo = {
   name: string
   age: string
@@ -12,5 +34,5 @@ type Bar = {
 
 type cases = [
   Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
-  Expect<Equal<Diff<Bar,Foo>, { gender: number }>>
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>
 ]
