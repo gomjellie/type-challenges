f0f88f78bfb0057f2a41a366e7ae6e7945fa7b3f
gomjellie <gomjellie@gmail.com>
2022-05-21 17:16:01 +0900

    2688-medium-startswith

--- questions/2688-medium-startswith/test-cases.ts
+++ questions/2688-medium-startswith/test-cases.ts
@@ -1,7 +1,11 @@
-import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+import { Equal, Expect } from '@type-challenges/utils'
+
+// prettier-ignore
+type StartsWith<T extends string, U extends string>
+  = T extends `${U}${infer Rest}` ? true : false
 
 type cases = [
   Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
   Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
-  Expect<Equal<StartsWith<'abc', 'abcd'>, false>>,
+  Expect<Equal<StartsWith<'abc', 'abcd'>, false>>
 ]
