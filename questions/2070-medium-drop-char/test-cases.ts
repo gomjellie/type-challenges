0e40d4940887e94e8543971d5ac329ab283946ce
gomjellie <gomjellie@gmail.com>
2022-05-21 16:01:32 +0900

    2070-medium-drop-char

--- questions/2070-medium-drop-char/test-cases.ts
+++ questions/2070-medium-drop-char/test-cases.ts
@@ -1,4 +1,10 @@
-import { Equal, Expect } from '@type-challenges/utils';
+import { Equal, Expect } from '@type-challenges/utils'
+
+// prettier-ignore
+type DropChar<S extends string, T extends string> =  
+  S extends `${infer F}${infer Rest}` ? (
+    F extends T ? `${DropChar<Rest, T>}` : `${F}${DropChar<Rest, T>}`
+  ) : ''
 
 type cases = [
   // @ts-expect-error
@@ -8,5 +14,5 @@ type cases = [
   Expect<Equal<DropChar<'    butter fly!        ', ' '>, 'butterfly!'>>,
   Expect<Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
   Expect<Equal<DropChar<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
-  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>
 ]
