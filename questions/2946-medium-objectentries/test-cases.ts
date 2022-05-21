e1b46b46739bf945dc6f37bdd9acf3101566ec81
gomjellie <gomjellie@gmail.com>
2022-05-21 18:19:21 +0900

    2946-medium-objectentries

--- questions/2946-medium-objectentries/test-cases.ts
+++ questions/2946-medium-objectentries/test-cases.ts
@@ -1,18 +1,31 @@
-import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+import { Equal, Expect } from '@type-challenges/utils'
 
 interface Model {
-  name: string;
-  age: number;
-  locations: string[] | null;
+  name: string
+  age: number
+  locations: string[] | null
 }
 
-type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null];
+type ModelEntries =
+  | ['name', string]
+  | ['age', number]
+  | ['locations', string[] | null]
 
+// prettier-ignore
+type RemoveUndefined<T> = 
+  [T] extends [undefined] ? undefined : (
+    T extends undefined ? never : T
+  )
 
+type ObjectEntries<T> = keyof T extends infer K
+  ? K extends unknown
+    ? [K, RemoveUndefined<T[K extends keyof T ? K : never]>]
+    : never
+  : never
 
 type cases = [
-  Expect<Equal<ObjectEntries<Model>,ModelEntries>>,
-  Expect<Equal<ObjectEntries<Partial<Model>>,ModelEntries>>,
-  Expect<Equal<ObjectEntries<{ key?: undefined}>, ['key', undefined]>>,
-  Expect<Equal<ObjectEntries<{ key: undefined}>, ['key', undefined]>>
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ['key', undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ['key', undefined]>>
 ]
