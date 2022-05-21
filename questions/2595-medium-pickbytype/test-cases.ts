3566618709e77882e27ff9349033ba0313d6f315
gomjellie <gomjellie@gmail.com>
2022-05-21 17:13:17 +0900

    2595-medium-pickbytype

--- questions/2595-medium-pickbytype/test-cases.ts
+++ questions/2595-medium-pickbytype/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// prettier-ignore
+type PickByType<T extends Model, U> = 
+  T extends unknown ? {
+    [k in keyof T as T[k] extends U ? k : never]: T[k]
+  } : never
+
 interface Model {
   name: string
   count: number
@@ -8,7 +14,12 @@ interface Model {
 }
 
 type cases = [
-  Expect<Equal<PickByType<Model, boolean>, { isReadonly: boolean; isEnable: boolean }>>,
+  Expect<
+    Equal<
+      PickByType<Model, boolean>,
+      { isReadonly: boolean; isEnable: boolean }
+    >
+  >,
   Expect<Equal<PickByType<Model, string>, { name: string }>>,
-  Expect<Equal<PickByType<Model, number>, { count: number }>>,
+  Expect<Equal<PickByType<Model, number>, { count: number }>>
 ]
