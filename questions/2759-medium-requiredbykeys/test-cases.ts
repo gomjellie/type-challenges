aaa4c2bf1dba8361e5d663e7ba4394bc6097be84
gomjellie <gomjellie@gmail.com>
2022-05-21 17:44:43 +0900

    2759-medium-requiredbykeys

--- questions/2759-medium-requiredbykeys/test-cases.ts
+++ questions/2759-medium-requiredbykeys/test-cases.ts
@@ -1,4 +1,4 @@
-import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+import { Equal, Expect } from '@type-challenges/utils'
 
 interface User {
   name?: string
@@ -9,18 +9,31 @@ interface User {
 interface UserRequiredName {
   name: string
   age?: number
-  address?: string 
+  address?: string
 }
 
 interface UserRequiredNameAndAge {
   name: string
   age: number
-  address?: string 
+  address?: string
 }
+
+type VisionMap<T> = {
+  [k in keyof T]: T[k]
+}
+
+// prettier-ignore
+type RequiredByKeys<T, K=never> = 
+  [K] extends [never] ? Required<T> : (
+      VisionMap<{
+        [k in keyof T as k extends K ? k : never]-?: T[k]
+      } & Omit<T, K extends keyof T ? K : never>
+    >
+  )
 
 type cases = [
   Expect<Equal<RequiredByKeys<User, 'name'>, UserRequiredName>>,
   Expect<Equal<RequiredByKeys<User, 'name' | 'unknown'>, UserRequiredName>>,
   Expect<Equal<RequiredByKeys<User, 'name' | 'age'>, UserRequiredNameAndAge>>,
-  Expect<Equal<RequiredByKeys<User>, Required<User>>>,
+  Expect<Equal<RequiredByKeys<User>, Required<User>>>
 ]
