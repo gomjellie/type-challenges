eff75915cd391563c440f4c7f8e3feb7423e4783
gomjellie <gomjellie@gmail.com>
2022-05-21 17:34:17 +0900

    2757-medium-partialbykeys

--- questions/2757-medium-partialbykeys/test-cases.ts
+++ questions/2757-medium-partialbykeys/test-cases.ts
@@ -1,4 +1,4 @@
-import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+import { Equal, Expect } from '@type-challenges/utils'
 
 interface User {
   name: string
@@ -9,18 +9,31 @@ interface User {
 interface UserPartialName {
   name?: string
   age: number
-  address: string 
+  address: string
 }
 
 interface UserPartialNameAndAge {
   name?: string
   age?: number
-  address: string 
+  address: string
 }
+
+type VisionMap<T> = T extends unknown
+  ? {
+      [k in keyof T]: T[k]
+    }
+  : never
+
+// prettier-ignore
+type PartialByKeys<T, K=never> = 
+  [K] extends [never] ? Partial<T> : 
+  VisionMap<{
+    [k in keyof T as k extends K ? k : never]+?: T[k]
+  } & Omit<T, K extends keyof T ? K : never>>
 
 type cases = [
   Expect<Equal<PartialByKeys<User, 'name'>, UserPartialName>>,
   Expect<Equal<PartialByKeys<User, 'name' | 'unknown'>, UserPartialName>>,
   Expect<Equal<PartialByKeys<User, 'name' | 'age'>, UserPartialNameAndAge>>,
-  Expect<Equal<PartialByKeys<User>, Partial<User>>>,
+  Expect<Equal<PartialByKeys<User>, Partial<User>>>
 ]
