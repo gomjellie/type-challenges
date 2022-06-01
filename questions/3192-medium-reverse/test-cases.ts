47f225377d0e0b4d0fe83abc4481218f6ee6ac65
gomjellie <gomjellie@gmail.com>
2022-06-01 14:41:07 +0900

    3192-medium-reverse

--- questions/3192-medium-reverse/test-cases.ts
+++ questions/3192-medium-reverse/test-cases.ts
@@ -1,5 +1,11 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+// prettier-ignore
+type Reverse<T extends unknown[]> = 
+  T extends [infer F, ...infer Rest] ? (
+    [...Reverse<Rest>, F]
+  ) : []
+
 type cases = [
   Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
   Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>
