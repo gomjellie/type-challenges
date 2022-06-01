b635a584191f4ff62726a91e74c4344cdb001c9e
gomjellie <gomjellie@gmail.com>
2022-06-01 14:58:28 +0900

    3196-medium-flip-arguments

--- questions/3196-medium-flip-arguments/test-cases.ts
+++ questions/3196-medium-flip-arguments/test-cases.ts
@@ -1,7 +1,26 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
+// prettier-ignore
+type Reverse<T extends unknown[]> = 
+  T extends [infer F, ...infer Rest] ? (
+    [...Reverse<Rest>, F]
+  ) : []
+
+// prettier-ignore
+type FlipArguments<T> = 
+  T extends (...args: infer A) => infer R ? (
+    A extends unknown[] ? (...args: Reverse<A>) => R : T
+  ) : T
+
 type cases = [
   Expect<Equal<FlipArguments<() => boolean>, () => boolean>>,
-  Expect<Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>>,
-  Expect<Equal<FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>, (arg0: boolean, arg1: number, arg2: string) => void>>
+  Expect<
+    Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>
+  >,
+  Expect<
+    Equal<
+      FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>,
+      (arg0: boolean, arg1: number, arg2: string) => void
+    >
+  >
 ]
